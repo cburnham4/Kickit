f6456533e68d7c1e535ce6519e441dfbd78507fd
Chase Burnham <cburnhamiv@gmail.com>
2016-01-26 11:40:23 -0500

    set up create post activity

--- app/src/main/java/letshangllc/kickit/CreateNewPost/CreatePostActivity.java
+++ app/src/main/java/letshangllc/kickit/CreateNewPost/CreatePostActivity.java
@@ -3,19 +3,41 @@ package letshangllc.kickit.CreateNewPost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import letshangllc.kickit.R;
 
 public class CreatePostActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private EditText et_category, et_message;
+
+    private Button btn_post_message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_post);
+        setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        et_category = (EditText) findViewById(R.id.et_category);
+        et_message = (EditText) findViewById(R.id.et_describe_activity);
+        btn_post_message = (Button) findViewById(R.id.btn_post_message);
+
+
+        btn_post_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String category = et_category.getText().toString();
+                String message = et_message.getText().toString();
+                /*Todo post to server */
+            }
+        });
     }
+
+
 }
